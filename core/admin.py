d01cbd8eb95cbc4c59f8729bf014929fde46de3f
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-11-10 22:28:22 -0300

    Aula 23

--- core/admin.py
+++ core/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 from core.models import Autor, Categoria, Editora, Livro, Usuario
 
-admin.site.register(Categoria)
-admin.site.register(Editora)
-admin.site.register(Autor)
-admin.site.register(Livro)
+# admin.site.register(Categoria)
+# admin.site.register(Editora)
+# admin.site.register(Autor)
+# admin.site.register(Livro)
 
 
 from django.contrib.auth.admin import UserAdmin
@@ -33,3 +33,32 @@ class UsuarioAdmin(UserAdmin):
     )
 
 admin.site.register(Usuario, UsuarioAdmin)
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    list_filter = ('nome',)
+    ordering = ('nome',)
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editora', 'categoria')
+    search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
+    list_filter = ('editora', 'categoria')
+    ordering = ('titulo', 'editora', 'categoria')
+    list_per_page = 25
